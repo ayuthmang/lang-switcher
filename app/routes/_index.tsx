edd7189e91323ad3ec68821ce78e9c584388645d
Ayuth Mangmesap <ayuth.mang@gmail.com>
2025-03-07 19:44:35 +0700

    feat: swap langs

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,10 @@
 import type { MetaFunction } from "@remix-run/node";
 import { useCallback, useDeferredValue, useState } from "react";
-import { EN_TH } from "~/constants/key-mapping";
+import { EN_TH, TH_EN } from "~/constants/key-mapping";
 import { cn } from "~/utils/misc";
 import { Textarea } from "~/components/ui/textarea";
 import debounce from "lodash/debounce";
+import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -15,6 +16,12 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+type EditorState = {
+  fromLang: "en" | "th";
+  toLang: "en" | "th";
+  transformer: (text: string) => string;
+};
+
 function buildKeyMapper(mapping: Record<string, string>) {
   return (text: string) => {
     return text
@@ -24,18 +31,23 @@ function buildKeyMapper(mapping: Record<string, string>) {
   };
 }
 
-const toThai = buildKeyMapper(EN_TH);
+const EDITOR_STATE_MAPPER: Record<string, EditorState> = {
+  en_th: { fromLang: "en", toLang: "th", transformer: buildKeyMapper(EN_TH) },
+  th_en: { fromLang: "th", toLang: "en", transformer: buildKeyMapper(TH_EN) },
+};
 
 function useEditorState() {
   const [fromText, setFromText] = useState("");
   const [toText, setToText] = useState("");
   const deferredToText = useDeferredValue(toText);
+  const [editorState, setEditorState] = useState<EditorState>(
+    EDITOR_STATE_MAPPER.en_th,
+  );
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const debouncedToThai = useCallback(
+  const debouncedToTargetLang = useCallback(
     debounce((text: string) => {
-      const toText = toThai(text);
-      setToText(toText);
+      setToText(editorState.transformer(text));
     }),
     [],
   );
@@ -44,47 +56,68 @@ function useEditorState() {
     (e: React.ChangeEvent<HTMLTextAreaElement>) => {
       const { value } = e.target;
       setFromText(value);
-      debouncedToThai(value);
+      debouncedToTargetLang(value);
     },
-    [debouncedToThai],
+    [debouncedToTargetLang],
   );
+
+  function swapLangState() {
+    setEditorState((prev) => ({
+      fromLang: prev.toLang,
+      toLang: prev.fromLang,
+      transformer:
+        EDITOR_STATE_MAPPER[`${prev.toLang}_${prev.fromLang}`].transformer,
+    }));
+    const prevToText = toText;
+    setFromText(prevToText);
+    setToText(fromText);
+  }
 
   return {
     fromText,
     setFromText,
     toText: deferredToText,
     handleFromTextChange,
+    langState: editorState,
+    swapLangState,
   };
 }
 
 export default function Index() {
-  const { fromText, toText, handleFromTextChange } = useEditorState();
+  const { fromText, toText, handleFromTextChange, langState, swapLangState } =
+    useEditorState();
 
   return (
     <main className="flex flex-1 flex-grow-[3]">
-      <div className="mx-auto flex max-w-7xl flex-1 flex-col gap-4 p-8 sm:flex-row sm:gap-8">
-        <FormGroup>
-          <label htmlFor="from">From</label>
-          <Textarea
-            className="h-full w-full flex-1"
-            id="from"
-            name="from"
-            value={fromText}
-            onChange={handleFromTextChange}
-            placeholder="Type something in English that you typed in Thai&#10;e.g., l;ylfu"
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="to">To</label>
-          <Textarea
-            className="h-full w-full flex-1"
-            id="to"
-            name="to"
-            value={toText}
-            readOnly
-            placeholder="See the magic happens here&#10;e.g., สวัสดี"
-          />
-        </FormGroup>
+      <div className="mx-auto flex max-w-7xl flex-1 flex-col gap-4 p-8 sm:flex-col sm:gap-8">
+        <div className="">
+          <Button onClick={swapLangState}>Swap Lang</Button>
+        </div>
+
+        <div className="flex flex-1 flex-col gap-4 sm:flex-row sm:gap-8">
+          <FormGroup>
+            <label htmlFor="from">From ({langState.fromLang})</label>
+            <Textarea
+              className="h-full w-full flex-1"
+              id="from"
+              name="from"
+              value={fromText}
+              onChange={handleFromTextChange}
+              placeholder="Type something in English that you typed in Thai&#10;e.g., l;ylfu"
+            />
+          </FormGroup>
+          <FormGroup>
+            <label htmlFor="to">To ({langState.toLang})</label>
+            <Textarea
+              className="h-full w-full flex-1"
+              id="to"
+              name="to"
+              value={toText}
+              readOnly
+              placeholder="See the magic happens here&#10;e.g., สวัสดี"
+            />
+          </FormGroup>
+        </div>
       </div>
     </main>
   );
